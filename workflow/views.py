7d6a1a01b855dc90da1f9c63ab22bc76a89fdb7f
belokongunner <byasic@gmail.com>
2017-01-23 12:56:08 +0200

    equals

--- workflow/views.py
+++ workflow/views.py
@@ -17,11 +17,12 @@ def issue(request, prkey, issuekey):
     })
 
 
-def profile(request, prkey, userkey):
+def profile(request, userkey):
     current_user = get_object_or_404(User, pk=userkey)
     return render(request, 'workflow/profile.html', {
         'user': current_user,
     })
+
 
 def project(request, prkey):
     projects = Project.objects.filter(project=prkey)
@@ -34,10 +35,9 @@ class SprintView(DetailView):
     model = Sprint
     template_name = 'workflow/sprint.html'
     query_pk_and_slug = True
-    pk_url_kwarg =  'sprintkey'
+    pk_url_kwarg = 'sprintkey'
     slug_field = 'project'
     slug_url_kwarg = 'prkey'
-
 
     def get_context_data(self, **kwargs):
         context = super(SprintView, self).get_context_data(**kwargs)
@@ -48,5 +48,3 @@ class SprintView(DetailView):
         context['resolved_issues'] = Issue.objects.filter(status="resolved", project_id=self.kwargs['prkey'],
                                                           sprint_id=self.kwargs['sprintkey'])
         return context
-
-
