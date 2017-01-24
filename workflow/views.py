7ef063849271d1f442c9c8e886425d4cd1755be8
belokongunner <byasic@gmail.com>
2017-01-24 12:58:58 +0200

    ProjectList by Class

--- workflow/views.py
+++ workflow/views.py
@@ -10,13 +10,6 @@ def index(request):
     return render(request, 'workflow/index.html')
 
 
-def issue(request, prkey, issuekey):
-    current_issue = get_object_or_404(Issue, pk=issuekey, project=prkey)
-    return render(request, 'workflow/issue.html', {
-        'issue': current_issue,
-    })
-
-
 def profile(request):
     current_user = request.user
     return render(request, 'workflow/profile.html', {
@@ -29,20 +22,4 @@ class ProjectListView(ListView):
     template_name = 'workflow/projects.html'
 
 
-class SprintView(DetailView):
-    model = Sprint
-    template_name = 'workflow/sprint.html'
-    query_pk_and_slug = True
-    pk_url_kwarg = 'sprintkey'
-    slug_field = 'project'
-    slug_url_kwarg = 'prkey'
 
-    def get_context_data(self, **kwargs):
-        context = super(SprintView, self).get_context_data(**kwargs)
-        context['new_issues'] = Issue.objects.filter(status="new", project_id=self.kwargs['prkey'],
-                                                     sprint_id=self.kwargs['sprintkey'])
-        context['in_progress_issues'] = Issue.objects.filter(status="in progress", project_id=self.kwargs['prkey'],
-                                                             sprint_id=self.kwargs['sprintkey'])
-        context['resolved_issues'] = Issue.objects.filter(status="resolved", project_id=self.kwargs['prkey'],
-                                                          sprint_id=self.kwargs['sprintkey'])
-        return context
