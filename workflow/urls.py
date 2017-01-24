7a1ece5f254dcf744f84746f0c6816402e67e332
belokongunner <byasic@gmail.com>
2017-01-24 12:39:27 +0200

    ProjectList by Class

--- workflow/urls.py
+++ workflow/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     url(r'^profile/', views.profile, name='profile'),
-    url(r'^project/', ProjectListView.as_view(), name='projects'),
+    url(r'^projects/', ProjectListView.as_view(), name='projects'),
     url(r'^project/(?P<prkey>[0-9]+)/issue/(?P<issuekey>[0-9]+)/$', views.issue, name='issue'),
     url(r'^project/(?P<prkey>[0-9]+)/sprint/(?P<sprintkey>[0-9]+)/$', views.SprintView.as_view(), name='sprint'),
 ]
