222b6360da67ee7b61db28ea677143c73796eb8c
italamanova <iryna.talamanova@gmail.com>
2017-01-26 22:07:53 +0200

    New markup + navbar include

--- workflow/tests.py
+++ workflow/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Project
+
+
+class ProjectMethodTests(TestCase):
+    def create_project(self, title='only a test',
+                       description='yes, this is only a test',
+                       start_date='2017-12-14', end_date='2017-12-14'):
+        return Project.objects.create(title=title, description=description,
+                                      start_date=start_date, end_date=end_date)
+
+    def test_project_creation(self):
+        test_project = self.create_project()
+        self.assertTrue(isinstance(test_project, Project))
+
+    # views (uses reverse)
+
+    # def test_project_list_view(self):
+    #     w = self.create_project()
+    #     url = reverse("workflow.views.ProjectListView")
+    #     resp = self.client.get(url)
+    #
+    #     self.assertEqual(resp.status_code, 200)
+    #     self.assertIn(w.title, resp.content)
+
+    # def test_project_detail(self):
+    #     response = self.client.get()
+    #     self.assertEqual(response.status_code, 200)
+
