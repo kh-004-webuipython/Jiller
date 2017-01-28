8b6755cb5d95b7376d13428472e89e16b9c633b2
Phobos <phobosprogrammer@gmail.com>
2017-01-28 13:12:10 +0200

    Change all regexp in urls to \d+

--- workflow/urls.py
+++ workflow/urls.py
@@ -6,15 +6,18 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
 
-    url(r'^project/(?P<pk>[0-9]+)/activesprint/$',
+    url(r'^project/(?P<pk>\d+)/activesprint/$',
         views.ActiveSprintViewtypeView.as_view(), name='active_sprint'),
 
-    url(r'^project/(?P<pk>\w+)/sprint/create/$',
+    url(r'^project/(?P<pk>\d+)/sprint/create/$',
         views.SprintCreate.as_view(), name='sprint_create'),
 
     #url for delete sprint. Hidden until create field is_active in Sprint model
-    #url(r'^project/(?P<pk>\w+)/sprint/delete/$',
+    #url(r'^project/(?P<pk>\d+)/sprint/delete/$',
     #views.SprintDelete.as_view(), name='sprint_delete'),
+
+    url(r'^issue/(?P<pk>\d+)/$',
+        views.SprintCreate.as_view(), name='sprint_create'),
 
     url(r'^login/$', views.login_form, name='login'),
     url(r'^registration/$', views.registration_form, name='registration'),
@@ -32,22 +35,22 @@ urlpatterns = [
     url(r'^project/delete/(?P<pk>\d+)/$', views.ProjectDelete.as_view(),
         name='project_delete'),
 
-    url(r'^project/(?P<project_id>[0-9]+)/backlog/$', views.backlog,
+    url(r'^project/(?P<project_id>\d+)/backlog/$', views.backlog,
         name='backlog'),
-    url(r'^project/(?P<project_id>\w+)/issue/create/$',
+    url(r'^project/(?P<project_id>\d+)/issue/create/$',
         views.create_issue, name='create_issue'),
-    url(r'^project/(?P<project_id>[0-9]+)/issue/(?P<issue_id>[0-9]+)/$',
+    url(r'^project/(?P<project_id>\d+)/issue/(?P<issue_id>\d+)/$',
         views.issue, name='issue'),
-    url(r'^project/(?P<project_id>\w+)/issue/(?P<issue_id>\w+)/edit/$',
+    url(r'^project/(?P<project_id>\d+)/issue/(?P<issue_id>\d+)/edit/$',
         views.edit_issue, name='edit_issue'),
-    url(r'^project/(?P<project_id>\w+)/team/$', views.team, name='team'),
-    url(r'^project/(?P<project_id>[0-9]+)/sprint/$',
+    url(r'^project/(?P<project_id>\d+)/team/$', views.team, name='team'),
+    url(r'^project/(?P<project_id>\d+)/sprint/$',
         views.sprints_list, name='sprints_list'),
-    url(r'^project/(?P<project_id>[0-9]+)/sprint/(?P<sprint_id>[0-9]+)/$',
+    url(r'^project/(?P<project_id>\d+)/sprint/(?P<sprint_id>\d+)/$',
         views.SprintView.as_view(), name='sprint'),
 
     url(r'^employee/$', views.employee_index_view, name='employee-index'),
-    url(r'^employee/(?P<employee_id>[0-9]+)/$', views.employee_detail_view,
+    url(r'^employee/(?P<employee_id>\d+)/$', views.employee_detail_view,
         name='employee-detail'),
 
 ]
