b73ab123c4cc8e818b4cade4c510e8d2d4d4b2f3
italamanova <iryna.talamanova@gmail.com>
2017-02-13 00:15:17 +0200

    replace assign_email files

--- general/tests.py
+++ general/tests.py
@@ -1,41 +1,52 @@
+from django.core import mail
 from django.test import Client
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from employee.models import Employee
+from project.forms import IssueForm, IssueFormForEditing
+from project.models import Project, Issue
 from project.tests import LoginRequiredBase
+
 
 class LoginViewTests(TestCase):
     def setUp(self):
         self.login_page = reverse('general:login')
         self.client = Client()
-        self.user = Employee.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword', first_name='Miss',
+        self.user = Employee.objects.create_user('john',
+                                                 'lennon@thebeatles.com',
+                                                 'johnpassword',
+                                                 first_name='Miss',
                                                  last_name='Mister')
 
     def test_for_correct_data(self):
         response = self.client.get(self.login_page)
         self.assertEqual(response.status_code, 200)
-        response = self.client.post(self.login_page, {'username': 'john', 'password': 'johnpassword'})
+        response = self.client.post(self.login_page, {'username': 'john',
+                                                      'password': 'johnpassword'})
         self.assertRedirects(response, reverse('general:profile'))
 
     def test_for_wrong_username(self):
         response = self.client.post(self.login_page,
-                                    {'username': 'wrongusername', 'password': 'johnpassword'})
+                                    {'username': 'wrongusername',
+                                     'password': 'johnpassword'})
         self.assertEqual(response.url, self.login_page)
         self.assertEqual(response.status_code, 302)
         response = self.client.get(response.url)
         self.assertContains(response, _('Wrong username or password'))
 
     def test_for_wrong_password(self):
-        response = self.client.post(self.login_page, {'username': 'john', 'password': 't2'})
+        response = self.client.post(self.login_page,
+                                    {'username': 'john', 'password': 't2'})
         self.assertEqual(response.url, self.login_page)
         self.assertEqual(response.status_code, 302)
         response = self.client.get(response.url)
         self.assertContains(response, _('Wrong username or password'))
 
     def test_for_empty_password(self):
-        response = self.client.post(self.login_page, {'username': 'john', 'password': ''})
+        response = self.client.post(self.login_page,
+                                    {'username': 'john', 'password': ''})
         self.assertEqual(response.url, self.login_page)
         self.assertEqual(response.status_code, 302)
         response = self.client.get(response.url)
@@ -60,14 +71,16 @@ class RegistrationViewTests(TestCase):
                 'email_confirmation': self.email,
                 'last_name': self.last_name, 'first_name': self.first_name}
 
-    def check_if_form_still_contain_info_after_error(self, response, expect_result):
+    def check_if_form_still_contain_info_after_error(self, response,
+                                                     expect_result):
         self.assertContains(response, expect_result['username'])
         self.assertContains(response, expect_result['email'])
         self.assertContains(response, expect_result['email_confirmation'])
         self.assertContains(response, expect_result['first_name'])
         self.assertContains(response, expect_result['last_name'])
         self.assertNotContains(response, expect_result['password'])
-        self.assertNotContains(response, expect_result['password_confirmation'])
+        self.assertNotContains(response,
+                               expect_result['password_confirmation'])
 
     def test_for_right_registration_data(self):
         response = self.client.get(self.registration_page_url)
@@ -104,11 +117,14 @@ class RegistrationViewTests(TestCase):
         response = self.client.post(self.registration_page_url, post_data)
         self.assertEqual(response.status_code, 200)
         self.check_if_form_still_contain_info_after_error(response, post_data)
-        self.assertContains(response, _('Password do not equal confirm password'))
+        self.assertContains(response,
+                            _('Password do not equal confirm password'))
 
     def test_username_already_exist(self):
         post_data = self.get_standard_post()
-        Employee.objects.create_user(post_data['username'], 'lennon@thebeatles.com', 'johnpassword', first_name='Miss',
+        Employee.objects.create_user(post_data['username'],
+                                     'lennon@thebeatles.com', 'johnpassword',
+                                     first_name='Miss',
                                      last_name='Mister')
 
         response = self.client.get(self.registration_page_url)
@@ -116,11 +132,13 @@ class RegistrationViewTests(TestCase):
         self.assertContains(response, _("Registration Form"))
         response = self.client.post(self.registration_page_url, post_data)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, _('User with this username already exists'))
+        self.assertContains(response,
+                            _('User with this username already exists'))
 
     def test_email_already_exist(self):
         post_data = self.get_standard_post()
-        Employee.objects.create_user('john', post_data['email'], 'johnpassword', first_name='Miss',
+        Employee.objects.create_user('john', post_data['email'],
+                                     'johnpassword', first_name='Miss',
                                      last_name='Mister')
         response = self.client.get(self.registration_page_url)
         self.assertEqual(response.status_code, 200)
@@ -129,16 +147,16 @@ class RegistrationViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, _('User with this email already exists'))
 
-    # def test_wrong_user_role(self):
-    #     post_data = self.get_standard_post()
-    #     post_data['role'] += '1'
-    #     response = self.client.get(self.registration_page_url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, _("Registration Form"))
-    #     response = self.client.post(self.registration_page_url, post_data)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.check_if_form_still_contain_info_after_error(response, post_data)
-    #     self.assertContains(response, _('Wrong user role'))
+        # def test_wrong_user_role(self):
+        #     post_data = self.get_standard_post()
+        #     post_data['role'] += '1'
+        #     response = self.client.get(self.registration_page_url)
+        #     self.assertEqual(response.status_code, 200)
+        #     self.assertContains(response, _("Registration Form"))
+        #     response = self.client.post(self.registration_page_url, post_data)
+        #     self.assertEqual(response.status_code, 200)
+        #     self.check_if_form_still_contain_info_after_error(response, post_data)
+        #     self.assertContains(response, _('Wrong user role'))
 
 
 class ProfileViewTests(LoginRequiredBase):
@@ -147,11 +165,32 @@ class ProfileViewTests(LoginRequiredBase):
         self.assertContains(response, 'Miss', status_code=200)
 
     def test_profile_view_with_incorrect_user(self):
-        self.user = Employee.objects.create_user('mark', 'webber@redbull.com', 'markpassword', first_name='Kiss',
+        self.user = Employee.objects.create_user('mark', 'webber@redbull.com',
+                                                 'markpassword',
+                                                 first_name='Kiss',
                                                  last_name='Dismiss')
         response = self.client.get(reverse('general:profile'))
         self.assertNotContains(response, 'Kiss')
 
 
 # class AssignEmailTests(LoginRequiredBase):
-#     def
+#     def setUp(self):
+#         super(AssignEmailTests, self).setUp()
+#         self.project = Project.objects.create()
+#         self.employee = Employee.objects.create()
+#         self.issue = Issue.objects.create(project=self.project,
+#                                           author=self.employee)
+#
+#     def test_check_assign_email(self):
+#         form_data = {'root': self.issue, 'employee': self.user,
+#                      'title': 'new issue', 'description': 'description',
+#                      'status': Issue.IN_PROGRESS, 'sprint': None
+#                      }
+#         issue_form = IssueFormForEditing(project=self.project, data=form_data, instance=self.issue)
+#         issue_form.send_email(self.user.email, self.employee.id, self.issue.id)
+#         self.assertEqual(len(mail.outbox), 1)
+#         self.assertEqual(mail.outbox[0].subject, 'Jiller notification')
+#         self.assertEqual(mail.outbox[0].from_email, self.employee.email)
+#         self.assertEqual(mail.outbox[0].to, [self.user.email])
+#         # self.assertEqual(outbox[0].body,
+#         #                  'Your password reset token:\n\n\t%s' % token.key)
