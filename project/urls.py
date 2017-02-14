661bae35069e9f99fbbd5ba2dd28f2bfdd646133
Norfolkik <norfolknorfolks@gmail.com>
2017-02-14 20:28:50 +0200

    Merge conflicts

--- project/urls.py
+++ project/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
         views.ActiveSprintDetailView.as_view(), name='sprint_active'),
     url(r'^(?P<project_id>\d+)/sprint/active/finish$',
         views.finish_active_sprint_view, name='finish_active_sprint'),
-
     url(r'^(?P<project_id>\d+)/(?P<issue_id>\d+)/push/$',
         views.push_issue_in_active_sprint, name='issue_push'),
 
