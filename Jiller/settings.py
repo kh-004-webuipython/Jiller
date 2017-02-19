9907334c11a999c870f2608b0256811a1ff36590
Vitalli <theoblivion@mail.ru>
2017-02-19 15:02:56 +0200

    hotfix

--- Jiller/settings.py
+++ Jiller/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'Jiller.middleware.LoginRequiredMiddleware.LoginRequiredMiddleware',
+    'Jiller.middleware.LoginRequiredMiddleware.LoginRequiredMiddleware',
     'Jiller.middleware.CheckProjectRelationMiddleware.CheckProjectRelation',
 ]
 
