a0cfee73a45df5cfa58a885ef794b7f4ddcb3ff1
italamanova <iryna.talamanova@gmail.com>
2017-02-21 00:43:19 +0200

    bad comments are deleted

--- general/tasks.py
+++ general/tasks.py
@@ -5,13 +5,11 @@ from general.emails import send_assign_email, send_email_after_sprint_start
 
 @task(name="send_assign_email_task")
 def send_assign_email_task(email, user_id, issue_id):
-    """sends an email when feedback form is filled successfully"""
     return send_assign_email( email, user_id, issue_id)
 
 
 @task(name="send_email_after_sprint_start_task")
 def send_email_after_sprint_start_task(email, user_id, sprint_id):
-    """sends an email when feedback form is filled successfully"""
     return send_email_after_sprint_start(email, user_id, sprint_id)
 
 
