8b67ef56c6c775e5f09d9f76a23860f315cda1fb
Vitalli <theoblivion@mail.ru>
2017-02-27 19:05:52 +0200

    right number of logs

--- project/templatetags/visible_logs.py
+++ project/templatetags/visible_logs.py
@@ -4,5 +4,7 @@ register = template.Library()
 
 
 @register.filter(name='visible_logs')
-def visible_logs(logs):
+def visible_logs(logs, count=False):
+    if count:
+        return logs.filter(is_hidden=False).count()
     return logs.filter(is_hidden=False)
