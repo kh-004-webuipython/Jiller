157fe528f1b35afab8c636ee433cc3130ed3a361
egepsihora <zeland18@ukr.net>
2017-03-27 16:44:40 +0300

    create room in db

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+
 import os
 import sqlite3
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
@@ -20,58 +21,39 @@ room_db = PyMongo(app)
 socketio = SocketIO(app)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def main_page():
-    if request.method == 'POST':
-        issue_json = request.get_json(force='True')
-        room = room_db.db.rooms
-        q = room.find_one({'url': issue_json['url']})
-        if q:
-            room.update(issue_json)
-        else:
-            issue_json['story_point'] = 0
-            for line in issue_json['team']:
-                line['estimate'] = 0
-            room.insert(issue_json)
     return render_template('index.html')
 
 
-@socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
+@app.route('/create_room/', methods=['POST'])
+def create_room():
+    issue_json = request.get_json(force='True')
+    room = room_db.db.rooms
+    q = room.find_one({'title': issue_json["title"]})
+    # room.update_one(q, {'$set': issue_json}, upsert=True)
+    if not q:
+        for line in issue_json['team']:
+            line['estimate'] = 0
+        issue_json['issues'] = []
+        #issue_json["link"] = request.url + str(issue_json["title"]) + "/"
+        room.insert(issue_json)
+    team = issue_json['team']
+    return render_template('index.html')
 
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
-
-
-@app.route('/link/')
-@app.route('/link/<name>')
-def main_page1(name=None):
-    return render_template('index.html', link=name)
-
-
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    emit('start playing', json)
-
-
-# @socketio.on('URI_link')
-# def handle_json(json):
-#     print('received json: ' + str(json))
-#     send(json, json=True, namespace='URI_link')
+@app.route('/add_issue/', methods=['POST'])
+def add_issue():
+    issue_json = request.get_json(force='True')
+    room = room_db.db.rooms
+    q = room.find_one({'title': issue_json["title"]})
+    room.update_one(q, {'$set': issue_json['issue']}, upsert=True)
 
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
-    print('received json: ' + str(json))
+    print('receive smth:' + str(json))
+    emit('resp', json, broadcast=True)
 
 
 if __name__ == '__main__':
